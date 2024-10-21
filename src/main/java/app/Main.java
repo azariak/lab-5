332d4c42868017198f85aeedb369d9099c64e559
bentenroadrunner <danielrafailov180@gmail.com>
2024-10-21 09:40:27 -0400

    Main.java todo complete

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,7 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
-        // TODO: add the Logout Use Case to the app using the appBuilder
+        // TODO: add the Logout Use Case to the app using the appBuilder DONE
         final JFrame application = appBuilder
                                             .addLoginView()
                                             .addSignupView()
@@ -20,6 +20,8 @@ public class Main {
                                             .addSignupUseCase()
                                             .addLoginUseCase()
                                             .addChangePasswordUseCase()
+                                            .addLogoutUseCase()
+                                            // added this line above
                                             .build();
 
         application.pack();
