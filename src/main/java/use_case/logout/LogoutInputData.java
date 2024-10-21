f7e18415131a8ba921e692a7a8c77938ca1e1718
Azaria <askelman5@gmail.com>
2024-10-21 12:24:29 -0400

    Finished TODO's in LogoutInteractor.java. All tests passing

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -5,8 +5,15 @@ package use_case.logout;
  */
 public class LogoutInputData {
 
+    private final String username;
+
+    public String getUsername() {
+        return username;
+    }
+
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
+        // TODO: save the current username in an instance variable and add a getter. DONE
+        this.username = username;
     }
 
 }
