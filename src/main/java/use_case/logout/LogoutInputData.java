5ae053232c467fdee60d883615cd55ede0d03016
Azaria <askelman5@gmail.com>
2024-10-22 11:11:47 -0400

    Fixed CheckStyle errors in LogoutInputData.java

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -7,13 +7,13 @@ public class LogoutInputData {
 
     private final String username;
 
-    public String getUsername() {
-        return username;
-    }
-
     public LogoutInputData(String username) {
         // TODO: save the current username in an instance variable and add a getter. DONE
         this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
 }
